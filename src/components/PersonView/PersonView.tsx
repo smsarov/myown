3d61047c915dc18cf6c8e4636e7f59d0ac834dfa
smsarov <smsarov@gmail.com>
2024-10-24 01:18:03 +0300

    add monkey

--- src/components/PersonView/PersonView.tsx
+++ src/components/PersonView/PersonView.tsx
@@ -5,14 +5,23 @@ import styles from "./personView.module.css";
 import Button from "../ui/Button";
 import AddNote from "./AddNote";
 import ViewNotes from "./ViewNotes";
-import { usePersonSelected, usePersonSelectedChange } from "../../contexts/PersonContext";
+import {
+  usePersonSelected,
+  usePersonSelectedChange,
+} from "../../contexts/PersonContext";
+import monkey from "../../assets/monkey-monkeytype.gif";
 
 function PersonView() {
   const [mode, setMode] = useState<"write" | "view">("write");
   const person = usePersonSelected();
   const changeSelectedPerson = usePersonSelectedChange();
 
-  if (!person) return <></>;
+  if (!person)
+    return (
+      <div style={{flexGrow: 1, display: "grid", placeItems: 'center'}}>
+        <img src={monkey} width={200} height={200}></img>
+      </div>
+    );
 
   return (
     <div className={styles.container}>
@@ -33,8 +42,15 @@ function PersonView() {
         >
           <Menu>
             <Button onClick={() => changeSelectedPerson()}>закрыть</Button>
-            <Button onClick={() => setMode("view")} selected={mode === "view"}>посмотреть</Button>
-            <Button onClick={() => setMode("write")} selected={mode === "write"}>написать</Button>
+            <Button onClick={() => setMode("view")} selected={mode === "view"}>
+              посмотреть
+            </Button>
+            <Button
+              onClick={() => setMode("write")}
+              selected={mode === "write"}
+            >
+              написать
+            </Button>
           </Menu>
         </div>
       </div>
